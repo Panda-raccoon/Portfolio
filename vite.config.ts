a621160fdb93f517de32515b7ab0b1b04ef9ab24
Panda-racoon <millie3519@gmail.com>
2025-01-11 19:40:21 +0900

    chore: Vite 서버 포트 및 호스트 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,18 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react({
+//       babel: {
+//         plugins: ["@emotion/babel-plugin"],
+//       },
+//       jsxImportSource: "@emotion/react",
+//     }),
+//   ],
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -11,4 +26,8 @@ export default defineConfig({
       jsxImportSource: "@emotion/react",
     }),
   ],
+  server: {
+    host: "127.0.0.1", // IPv4 주소로 강제 설정
+    port: 3000, // 포트를 3000번으로 변경 (원하는 다른 포트 번호로도 변경 가능)
+  },
 });
